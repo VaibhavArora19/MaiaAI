07170596c1d34de4e3b61ec2c235564428ab6407
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-11-26 23:14:45 +0530

    add web3auth

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,20 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable no-console */
+
+"use client";
+
 import Navbar from "@/components/Navbar/Navbar";
 
-export default function Home() {
+// import RPC from "./viemRPC";
+// import RPC from "./web3RPC";
+// IMP END - Blockchain Calls
+
+function App() {
   return (
     <div className="">
       <Navbar />
     </div>
   );
 }
+
+export default App;
